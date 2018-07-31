4388c216ac50b1a30cf45b2dcbea65dee949e787
salehinRafi <salehinrafi35@gmail.com>
2018-07-31 12:00:42 +0800

    Categorized array, slice and operation for both (range) into one folder

---
+++ 4-arrays, slices, range/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("------Array Sections------")
+	Array()
+	fmt.Println("------Slices Sections------")
+	Slices()
+	fmt.Println("------Range Sections------")
+	Range()
+}
