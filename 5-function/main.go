5284599fff3d0aebb9f0f2c1b765aaeb61cf2dc3
salehinRafi <salehinrafi35@gmail.com>
2018-08-02 13:53:32 +0800

    Add Erroc Control Example

--- 5-function/main.go
+++ 5-function/main.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
+// Functions start with func and surround the code with { }
 func main() {
+
 	fmt.Println("------Basic Function Sections------")
 	Function()
 	fmt.Println("------Function As A Closure Sections------")
 	Closure()
-	fmt.Println("------Defer, Recover, Panic Sections------")
-	Defer()
+	fmt.Println("------Error Control Sections------")
+	ErrorControl()
 }
