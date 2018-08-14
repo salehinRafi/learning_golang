3ead733e7d3a48f674bba2a6b855d1112b2fa8f0
salehinRafi <salehinrafi35@gmail.com>
2018-08-14 16:19:41 +0800

    Add examples for many useful string-related functions in standard library’s `strings` package

---
+++ strings-package/main.go
@@ -0,0 +1,29 @@
+package main
+
+import s "strings" // alias strings to a shorter name.
+import "fmt"
+
+// We alias fmt.Println to a shorter name.
+var p = fmt.Println
+
+func main() {
+
+	p("Contains:  ", s.Contains("test", "es"))
+	p("Count:     ", s.Count("test", "t"))
+	p("HasPrefix: ", s.HasPrefix("test", "te"))
+	p("HasSuffix: ", s.HasSuffix("test", "st"))
+	p("Index:     ", s.Index("test", "e"))
+	p("Join:      ", s.Join([]string{"a", "b"}, "-"))
+	p("Repeat:    ", s.Repeat("a", 5))
+	p("Replace:   ", s.Replace("foo", "o", "0", -1))
+	p("Replace:   ", s.Replace("foo", "o", "0", 1))
+	p("Split:     ", s.Split("a-b-c-d-e", "-"))
+	p("ToLower:   ", s.ToLower("TEST"))
+	p("ToUpper:   ", s.ToUpper("test"))
+	p()
+
+	// getting the length of a string in bytes.
+	p("Len: ", len("hello"))
+	//  getting a byte by index.
+	p("Char:", "hello"[1])
+}
